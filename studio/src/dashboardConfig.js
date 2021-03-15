008a5f48502cc83a150feaf96d8ecd2969e360de
Sanity.io <hello@sanity.io>
2021-03-15 11:12:34 -0400

    Initial commit

---
+++ studio/src/dashboardConfig.js
@@ -0,0 +1,47 @@
+export default {
+  widgets: [
+    { name: 'structure-menu' },
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '604f796030d9eb0e65bb6150',
+                  title: 'Sanity Studio',
+                  name: 'sanity-gatsby-blog-studio-y7pvm4ww',
+                  apiId: 'dde5b68e-3b55-4a8a-89ba-f11e15994f12'
+                },
+                {
+                  buildHookId: '604f79604f3a6f0d9d5d24c3',
+                  title: 'Blog Website',
+                  name: 'sanity-gatsby-blog-web-3unry6a9',
+                  apiId: '52fe0e19-432e-4d6a-b09a-21f0438e0311'
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/MAU-Estate/sanity-gatsby-blog',
+            category: 'Code'
+          },
+          { title: 'Frontend', value: 'https://sanity-gatsby-blog-web-3unry6a9.netlify.app', category: 'apps' }
+        ]
+      }
+    },
+    { name: 'project-users', layout: { height: 'auto' } },
+    {
+      name: 'document-list',
+      options: { title: 'Recent blog posts', order: '_createdAt desc', types: ['post'] },
+      layout: { width: 'medium' }
+    }
+  ]
+}
